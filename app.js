915b38466d6d1a5f1dfb70bd4f7264780d3ef41f
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-01-29 21:14:26 +0900

    app.js mousemove

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas=document.getElementById("jsCanvas");
 const ctx=canvas.getContext("2d");
 
+canvas.width=700;
+canvas.height=700;
+
 ctx.strokeStyle="#2c2c2c";
 ctx.lineWidth=2.5;
 
@@ -22,6 +25,7 @@ function onMouseMove(event){
         ctx.moveTo(x,y);
     }else{
         ctx.lineTo(x,y);
+        ctx.stroke()
     }
 }
 
